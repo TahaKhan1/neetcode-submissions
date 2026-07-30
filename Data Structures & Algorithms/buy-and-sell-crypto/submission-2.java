ce06aae9d808de8284d47b24534e1b461e500347
Taha Khan <khantaha127@gmail.com>
2026-07-30 00:33:29 -0400

    Add: buy-and-sell-crypto - submission-2

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-2.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+
+        int left = 0;
+        int currentDifference = 0;
+        int maxDifference = Integer.MIN_VALUE;
+        for (int right=1; right<prices.length; right++){
+            
+            if(prices[right] < prices[left]){
+                left++;
+                prices[left] = prices[right];
+            }
+            currentDifference = prices[right] - prices[left];
+            maxDifference = Math.max(currentDifference, maxDifference);
+        }
+        if(maxDifference > 0){
+            return maxDifference;
+        }else{
+            return 0;
+        }
+    }
+}
